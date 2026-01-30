e37d3b92bb80179c79165901ee29827473df2f8c
SHAMUNESH P <shamuselvam2001@gmail.com>
2026-01-30 08:58:50 +0530

    delete,edit inputSignal added

--- src/app/components/contacts-list/contacts-list.component.ts
+++ src/app/components/contacts-list/contacts-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, computed, inject, resource, signal } from '@angular/core';
 import { Contact } from '../../model/contact';
 import { MatListModule } from '@angular/material/list';
 import { ApiService } from '../../services/api.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-contacts-list',
   imports: [
@@ -12,18 +13,25 @@ import { MatButtonModule } from '@angular/material/button';
     MatProgressSpinnerModule,
     MatIconModule,
     MatButtonModule,
+    RouterLink,
   ],
   templateUrl: './contacts-list.component.html',
   styleUrl: './contacts-list.component.scss',
 })
 export class ContactsListComponent {
   apiService = inject(ApiService);
+  deleting = signal(false);
+  loading = computed(
+    () => this.contactsResource.isLoading() || this.deleting(),
+  );
   contactsResource = resource({
     loader: () => this.apiService.getContacts(),
   });
 
   async deleteContact(id: string) {
+    this.deleting.set(true);
     await this.apiService.deleteContact(id);
+    this.deleting.set(false);
     this.contactsResource.reload();
   }
 }
